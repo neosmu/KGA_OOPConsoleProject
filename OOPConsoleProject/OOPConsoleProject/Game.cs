1e3891f135c0616ea1b66d7d40f3b4069f3f609b
neosmu <hojunglee00@naver.com>
2025-04-08 10:35:14 +0900

    [FEAT] Scene설정 및TitleScene구현
    씬 설정과 타이틀씬을 구현해 동작시킴

---
+++ OOPConsoleProject/OOPConsoleProject/Game.cs
@@ -0,0 +1,46 @@
+﻿using OOPConsoleProject.Scenes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPConsoleProject
+{
+    public static class Game
+    {
+        private static Dictionary<string, Scene> sceneDic;
+        private static Scene curScene;
+        private static bool gameOver;
+
+        // 게임 동작
+        public static void Run()
+        {
+            Start();
+            while (gameOver == false)
+            {
+                curScene.Render();
+                curScene.Input();
+                curScene.Update();
+                curScene.Result();
+            }
+            End();
+        }
+        // 게임 시작 설정
+        private static void Start()
+        {
+            // 게임 설정
+            gameOver = false;
+            // 씬 설정
+            sceneDic = new Dictionary<string, Scene>();
+            sceneDic.Add("Title", new TitleScene());
+
+            curScene = sceneDic["Title"];
+        }
+
+        private static void End()
+        {
+
+        }
+    }
+}
---
+++ OOPConsoleProject/OOPConsoleProject/Scenes/TitleScene.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPConsoleProject.Scenes
+{
+    public class TitleScene : Scene
+    {
+        public TitleScene()
+        {
+            name = "Title";
+        }
+        public override void Render()
+        {
+            Console.WriteLine("********************");
+            Console.WriteLine("*   몬스터 사냥    *");
+            Console.WriteLine("********************");
+            Console.WriteLine();
+            Console.WriteLine("계속하려면 아무키나 누르세요...");
+        }
+        public override void Input()
+        {
+            Console.ReadKey(true);
+        }
+        public override void Update()
+        {
+            
+        }
+        public override void Result()
+        {
+            
+        }
+    }
+}
